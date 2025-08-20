fba6dc37e74c6dd837658d1356a4668742023bf6
Evgeniy Gavrilov <evgeniy.gavrilov.dev@gmail.com>
2025-08-20 16:30:10 +0300

    Fixed config

--- src/main/java/com/medical_learning_platform/app/conf/CorsGlobalConfig.java
+++ src/main/java/com/medical_learning_platform/app/conf/CorsGlobalConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class CorsGlobalConfig {
+@EnableWebFlux
+public class CorsGlobalConfig implements WebFluxConfigurer {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
@@ -22,5 +26,15 @@ public class CorsGlobalConfig {
         source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("http://localhost:4200")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//            .allowedHeaders("*")
+//            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
